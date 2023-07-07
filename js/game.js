4b2a3fd1bcdd8f9cb5d7783c944d990e486ba6f1
Pablo <pablocavalcantee@gmail.com>
2023-07-06 21:16:15 -0300

    att

--- js/game.js
+++ js/game.js
@@ -86,7 +86,7 @@ const createCard = (character) => {
   const front = createElement('div', 'face front');
   const back = createElement('div', 'face back');
 
-  front.style.backgroundImage = `url('../pics/${character}.png')`;
+  front.style.backgroundImage = `url('..pics/${character}.png')`;
 
   card.appendChild(front);
   card.appendChild(back);
